b903bf49c489fbf74e7ca4f4bd3bb7be9be235f0
Maria-Zurakowska <maria.zurakowska@gmail.com>
2024-04-05 12:56:43 +0200

    Add int getMaxHoursInWeek(int index, int[][] array, int lastColumnIndex) and int getLastRowMaxHours(int index, int[][] array) methods

--- src/main/java/com/takeyouforward/recursion/re4/MaxHoursInWeek.java
+++ src/main/java/com/takeyouforward/recursion/re4/MaxHoursInWeek.java
@@ -5,19 +5,17 @@ public class MaxHoursInWeek {
     public static void main(String[] args) {
 
         int[][] array = {
-                {10, 7, 5},
-                {5, 8, 5},
-                {2, 9, 4},
-                {7, 10, 7},
-                {0, 2, 9},
-                {1, 5, 2}
+                {2,1,3},
+                {3,4,6},
+                {10,1,6},
+                {8,3,7}
         };
-        System.out.println(getMaxHoursInWeek(array.length - 1, array, 1));
+        System.out.println(getMaxHoursInWeek(array.length - 1, array, getLastRowMaxHours(array)));
     }
 
     public static int getMaxHoursInWeek(int index, int[][] array, int lastColumnIndex){
 
-         int maxValue = -1;
+        int maxValue = -1;
 
         if(index == 0){
             for(int j = 0; j < array[index].length; j++){
@@ -32,18 +30,18 @@ public class MaxHoursInWeek {
             for (int j = 0; j < array[index].length; j++) {
                 if (j != lastColumnIndex) {
 
-                    int thisDay = array[index][j] + getMaxHoursInWeek(index - 1, array, j);
+                    int thisDay = array[index][j] + getMaxHoursInWeek(index - 1, array, lastColumnIndex);
                     maxValue = Math.max(maxValue, thisDay);
                 }
             }
             return maxValue;
     }
 
-    public static int getLastRowMaxHours(int index, int[][] array){
+    public static int getLastRowMaxHours(int[][] array){
 
         int maxValue = -1;
 
-        for(int j = 0; j < array[index].length; j++){
+        for(int j = 0; j < array[array.length - 1].length; j++){
 
             int lastRowResult = getMaxHoursInWeek(array.length - 1, array, j);
             maxValue = Math.max(maxValue, lastRowResult);
